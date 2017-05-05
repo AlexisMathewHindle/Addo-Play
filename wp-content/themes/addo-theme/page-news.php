b45a9eb3ede6131730c226895a5b6a1ab9eae622
AlexisHindle <alexishindle@gmail.com>
2017-05-05 08:33:49 +0100

    Fixed news page

--- wp-content/themes/addo-theme/page-news.php
+++ wp-content/themes/addo-theme/page-news.php
@@ -10,39 +10,32 @@
 
 get_header();?>
 
-<div class="container">
-    <div class="row">
-        <?php
-            $args = array(
-                'post_type' => 'post'
-            );
 
-            $post_query = new WP_Query($args);
-            if($post_query->have_posts() ) {
-                while($post_query->have_posts() ) {
-                    $post_query->the_post();
-                    ?>
-                    <div class="col-md-6">
-                        <div class="news-archive">
-                        <h2><?php the_title(); ?></h2>
-                        <p><?php the_excerpt();?></p>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="news-archive-img">
-                        <?php echo get_the_post_thumbnail();?>
-                        </div>
-                    </div>
-                    
-                    <?php
-                }
-            }
-        ?>
+<div class="container">
+<?php $the_query = new WP_Query( 'posts_per_page=5' ); ?>
+
+<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="news-archive">
+            <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a><h2>
+            <?php the_excerpt(__('(more…)')); ?>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="news-archive-img">
+                <?php echo get_the_post_thumbnail();?>
+            </div>
+        </div>
     </div>
+
+<?php 
+endwhile;
+wp_reset_postdata();
+?>
 </div>
 
-
-
-	
 
 <?php get_footer();?>
